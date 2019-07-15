51e3a3376e63eb74cb86e22ecca42c07976b266c
Chirag-Gupta-057 <chiraggupta057@gmail.com>
2019-07-15 18:31:23 +0530

    added paytm payment gateway

--- travello/urls.py
+++ travello/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('pay_gate',views.payment,name='payment'),
     path('ticket',views.ticket,name='ticket'),
+    path('notification/', views.notificationfrompaytm, name ='notification'),
+    path('home',views.index,name='home')
    
 ]
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Destination,Payments
-# Create your views here.
+from paytm import Checksum
+from django.views.decorators.csrf import csrf_exempt
+import random
+import string
+
+def randomStringDigits(stringLength=10):
+    """Generate a random string of letters and digits """
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
 def index(request):
     dests = Destination.objects.all()
     return render(request,'index.html',{'dests':dests})
@@ -19,8 +28,31 @@ def payment(request):
         payments.Price=quantity*price
         payments.save()
 
+    MERCHANT_KEY = 'bQfzzkKzeCbR7jOl'
+    param_dict = {
+        'MID': 'amitgo59443067266036',
+        'ORDER_ID': randomStringDigits(), # order id 
+        'TXN_AMOUNT': str(price*quantity), # amount demanded for.
+        'CUST_ID': "chiragrocks998@gmail.com",
+        'INDUSTRY_TYPE_ID': 'Retail',
+        'WEBSITE': 'STAGING', # for demo purpose only.
+        'CHANNEL_ID': 'WEB',
+        'CALLBACK_URL':'http://127.0.0.1:8000/notification/' # on this url paytm will send you the status of request
+    };param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict,MERCHANT_KEY);
     
-    return render(request,'payment.html',{'payment_details':payments})
+    return render(request,'pay/makepay.html',{'data':param_dict})
+
+    
+    #return render(request,'payment.html',{'payment_details':payments})
 
 def ticket(request):
     return render(request,'contact.html')
+
+@csrf_exempt
+def notificationfrompaytm(request):
+    data = dict()
+    #id = request.session['id']
+    #print(id)
+    for k,v in request.POST.items():
+        data.setdefault(k,v)
+    return render(request,'pay/paymsg.html',data)
